973f87d85e3c18e71e36576487807e442b28e472
Hector Dearman <hector.dearman@gmail.com>
2022-01-23 15:14:57 -0800

    Add contact email address

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     fontFamily: {
       "display": ["Advocate", "ui-sans-serif", "system-ui"],
       "body": ["Equity", "ui-serif", "system-ui"],
+      "mono": ["ui-monospace", "SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation\\ Mono", "Courier\\ New", "monospace"],
     },
     extend: {},
   },
