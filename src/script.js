390f96c5a4c9ad0fed93880a7976445bd94a202e
Hector Dearman <hector.dearman@gmail.com>
2022-01-23 20:21:03 -0800

    Improve slide draw

--- src/script.js
+++ src/script.js
@@ -1,12 +1,19 @@
 
 const addMemoryButton = document.getElementById('add-memory');
 const hideSlideOutButton = document.getElementById('hide-slide-out')
+const cancelSlideOutButton= document.getElementById('cancel-slide-out')
 const slideOutOverlay = document.getElementById('slide-out-overlay')
 
-addMemoryButton.onclick = () => {
-  slideOutOverlay.style.setProperty("display", null);
-};
-hideSlideOutButton.onclick = () => {
-  slideOutOverlay.style.setProperty("display", "none");
-};
+function close() {
+  slideOutOverlay.classList.add("hidden");
+}
+
+function open() {
+  slideOutOverlay.classList.remove("hidden");
+}
+
+
+addMemoryButton.onclick = open;
+hideSlideOutButton.onclick = close;
+cancelSlideOutButton.onclick = close;
 
