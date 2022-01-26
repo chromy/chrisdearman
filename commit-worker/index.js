6fd2d24c81a513485ab4b270e0989184a4a0efea
Hector Dearman <hector.dearman@gmail.com>
2022-01-26 12:10:07 -0800

    More commit stuff

--- commit-worker/index.js
+++ commit-worker/index.js
@@ -1,8 +1,92 @@
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
-});
+
+async function github(query, variables) {
+  const r = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      "User-Agent": "chromy",
+      "Authorization": `bearer ${GITHUB_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+  const j = await r.json();
+  return j;
+}
+
+async function fetchRepoId() {
+  const query = `
+    query {
+      repository(owner: "chromy", name: "chrisdearman") {
+        id
+      }
+    }
+  `;
+  return (await github(query, {})).data.repository.id;
+}
+
+//async function createBranch(name) {
+//  const query = `
+//    mutation ($input: CreateRefInput!) {
+//      ref {
+//        name
+//      }
+//    }
+//  `;
+//  const variables = {
+//    "input": {
+//      "name": `refs/heads/${name}`,
+//
+//    },
+//  };
+//  const r = await graphql(query, variables);
+//  const j = await r.json();
+//}
+//
+//async function createMessage(name) {
+//  const query = `
+//    mutation ($input: CreateCommitOnBranchInput!) {
+//      createCommitOnBranchInput {
+//        commit {
+//          url
+//        }
+//      }
+//    }
+//  `;
+//  const variables = {
+//    "input": {
+//      "branch": {
+//        "repositoryNameWithOwner": "chromy/chrisdearman",
+//        "branchName": "foo",
+//      },
+//      "message": {
+//        "headline": "Hello, world!",
+//      },
+//      "fileChanges": {
+//        "additions" [
+//          {
+//            "path": "src/a.txt",
+//            "contents": Buffer.from("new content here\n").toString("base64"),
+//          }
+//        ],
+//      },
+//    },
+//  };
+//  const r = await graphql(query, variables);
+//  const j = await r.json();
+//}
 
 async function handleRequest(request) {
-  const access_token = GITHUB_ACCESS_TOKEN;
-  return new Response("Hello World");
+  const id = await fetchRepoId();
+  console.log(id);
+  return new Response(id);
 }
+
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+
