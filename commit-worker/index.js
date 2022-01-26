f214010722f993ad78bc4b3775df34662269f78c
Sam Szreter <s.szreter@gmail.com>
2022-01-26 15:48:14 -0600

    Create branch via graphql

--- commit-worker/index.js
+++ commit-worker/index.js
@@ -1,4 +1,3 @@
-
 async function github(query, variables) {
   const r = await fetch("https://api.github.com/graphql", {
     method: "POST",
@@ -17,35 +16,47 @@ async function github(query, variables) {
   return j;
 }
 
-async function fetchRepoId() {
+async function fetchRepo() {
   const query = `
     query {
       repository(owner: "chromy", name: "chrisdearman") {
         id
+        defaultBranchRef {
+          target {
+            oid
+          }
+        }
       }
     }
   `;
-  return (await github(query, {})).data.repository.id;
+  const { data: { repository } } = (await github(query, {}));
+  return {
+      repository_id: repository.id,
+      main_branch_node_id: repository.defaultBranchRef.target.oid,
+  };
 }
 
-//async function createBranch(name) {
-//  const query = `
-//    mutation ($input: CreateRefInput!) {
-//      ref {
-//        name
-//      }
-//    }
-//  `;
-//  const variables = {
-//    "input": {
-//      "name": `refs/heads/${name}`,
-//
-//    },
-//  };
-//  const r = await graphql(query, variables);
-//  const j = await r.json();
-//}
-//
+async function createBranch(name, repository) {
+  const query = `
+    mutation($input:CreateRefInput!) {
+      createRef(input: $input) {
+        ref {
+          name
+        }
+      }
+    }
+  `;
+  const variables = {
+    "input": {
+      "name": `refs/heads/${name}`,
+      "oid": repository.main_branch_node_id,
+      "repositoryId": repository.repository_id,
+    },
+  };
+  const data = await github(query, variables);
+  return data;
+}
+
 //async function createMessage(name) {
 //  const query = `
 //    mutation ($input: CreateCommitOnBranchInput!) {
@@ -80,9 +91,11 @@ async function fetchRepoId() {
 //}
 
 async function handleRequest(request) {
-  const id = await fetchRepoId();
-  console.log(id);
-  return new Response(id);
+  const repository = await fetchRepo();
+  console.log('BEFORE HANDLE', JSON.stringify(repository));
+  const newBranch = await createBranch('test_new_branch', repository);
+  console.log('AFTER HANDLE', JSON.stringify(newBranch));
+  return new Response(repository);
 }
 
 addEventListener("fetch", event => {
